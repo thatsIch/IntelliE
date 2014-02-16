37dbe179780d949c47e2e04396672f6d3457fad0
thatsIch <badhard@arcor.de>
2014-02-16 18:49:05 +0100

    Initial Commit

--- src/main/java/de/thatsich/appint/IntelliE.java
+++ src/main/java/de/thatsich/appint/IntelliE.java
@@ -10,7 +10,7 @@ import net.minecraft.init.Blocks;
 @Mod(modid = IntelliE.MODID, version = IntelliE.VERSION)
 public class IntelliE
 {
-	public static final String MODID = "examplemod";
+	public static final String MODID = "intellie";
 	public static final String VERSION = "1.0";
 
 	@EventHandler
