e6122cf5ca37eb26e7517047db2323bd9708ca0d
thatsIch <badhard@arcor.de>
2014-02-28 16:32:42 +0100

    Cleanup of Baseclass

--- src/main/java/de/thatsich/common/AGuiceMod.java
+++ src/main/java/de/thatsich/common/AGuiceMod.java
@@ -1,11 +1,9 @@
 package de.thatsich.common;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Module;
-import com.google.inject.binder.AnnotatedBindingBuilder;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -13,6 +11,7 @@ import de.thatsich.common.handler.RegistryBlock;
 import de.thatsich.common.handler.RegistryConfig;
 import de.thatsich.common.handler.RegistryItem;
 import de.thatsich.common.handler.RegistryTileEntity;
+import de.thatsich.common.util.Logger;
 import net.minecraftforge.common.MinecraftForge;
 
 import java.io.File;
@@ -21,13 +20,16 @@ import java.lang.reflect.Constructor;
 import java.util.Collection;
 import java.util.LinkedList;
 
-
+/**
+ * Minecraft Mod with enabled Guice Injection namely @Inject
+ */
 public abstract class AGuiceMod
 {
 	private final RegistryConfig configs;
 	private final RegistryBlock blocks;
 	private final RegistryItem items;
 	private final RegistryTileEntity tileEntites;
+	private final Logger log;
 
 	/**
 	 * To make sure that the initialization of Guice-based JavaFX application
@@ -36,74 +38,49 @@ public abstract class AGuiceMod
 	 */
 	protected AGuiceMod ()
 	{
-		super();
-
-		// Prepare
-		final Collection<Module> modules = new LinkedList<>();
-		final Injector tempInjector = Guice.createInjector();
-
-		@SuppressWarnings("unchecked")
-		final Class<AGuiceMod> clazz = (Class<AGuiceMod>) this.getClass();
-
-		this.isConstructorInjected( this );
-
-		modules.add( new AGuiceMod.bindInstanceClassModule( clazz ) );
-
-		// Propagates initialization of additional modules to the specific
-		// subclass of this GuiceApplication instance.
-		final Collection<Class<? extends AMinecraftModule>> additionalModuleClasses = this.initModules();
-		for ( Class<? extends Module> moduleClass : additionalModuleClasses )
+		if ( this.isConstructorInjected() )
 		{
-			final Module moduleInstance = tempInjector.getInstance( moduleClass );
-			modules.add( moduleInstance );
+			throw new IllegalStateException( "GuiceApplication with constructor that is marked with @Inject is not allowed!" );
 		}
 
 		// Creates an injector with all of the required modules.
+		final Collection<? extends Module> modules = this.getModuleInstances();
 		final Injector injector = Guice.createInjector( modules );
+
+		// Enable Logging
+		this.log = injector.getInstance( Logger.class );
 
 		// Injects all fields annotated with @Inject into this GuiceApplication instance.
 		injector.injectMembers( this );
 
-		this.configs = tempInjector.getInstance( RegistryConfig.class );
-		this.blocks = tempInjector.getInstance( RegistryBlock.class );
-		this.items = tempInjector.getInstance( RegistryItem.class );
-		this.tileEntites = tempInjector.getInstance( RegistryTileEntity.class );
+		// Inject all Registries
+		this.configs = injector.getInstance( RegistryConfig.class );
+		this.blocks = injector.getInstance( RegistryBlock.class );
+		this.items = injector.getInstance( RegistryItem.class );
+		this.tileEntites = injector.getInstance( RegistryTileEntity.class );
 		//		final RegistryEntity entities = tempInjector.getInstance( RegistryEntity.class );
 		//		final HandlerGui gui = tempInjector.getInstance( HandlerGui.class );
 	}
 
 	/**
-	 * This method is used to fetch and/or create (Guice) modules necessary
-	 * to fully construct this application.
-	 * <p>
-	 * The modules that are initialized in this method and added to the passed
-	 * List will be used to create the {@link com.google.inject.Injector} instance that is used in
-	 * the context of this application.
-	 * </p>
-	 *
-	 * @return A list of modules (initially empty) that shall be used to
-	 * create the injector to be used in the context of this application.
-	 */
-	public abstract Collection<Class<? extends AMinecraftModule>> initModules ();
-
-	/**
 	 * Checks the GuiceApplication instance and makes sure that none of the
 	 * constructors is annotated with @Inject!
-	 *
-	 * @param instance to be checked instance
 	 */
-	private boolean isConstructorInjected ( AGuiceMod instance )
+	private boolean isConstructorInjected ()
 	{
-		final Class<? extends AGuiceMod> classInstance = instance.getClass();
+		final Class<? extends AGuiceMod> classInstance = this.getClass();
+		boolean result = false;
+
 		for ( final Constructor<?> constructor : classInstance.getConstructors() )
 		{
 			if ( this.isInjectAnnotationPresent( constructor ) )
 			{
-				throw new IllegalStateException( "GuiceApplication with constructor that is marked with @Inject is not allowed!" );
+				result = true;
+				break;
 			}
 		}
 
-		return false;
+		return result;
 	}
 
 	/**
@@ -124,6 +101,47 @@ public abstract class AGuiceMod
 	}
 
 	/**
+	 * Propagates initialization of additional modules to the specific
+	 * subclass of this GuiceApplication instance.
+	 *
+	 * @return ModuleInstances
+	 */
+	private Collection<? extends Module> getModuleInstances ()
+	{
+		// Preprare
+		final Injector injector = Guice.createInjector();
+		final Collection<Module> instances = new LinkedList<>();
+		final Collection<Class<? extends AMinecraftModule>> classModules = new LinkedList<>();
+
+		// Fetch all ModuleClasses
+		this.initModules( classModules);
+
+		// Process
+		for ( Class<? extends Module> moduleClass : classModules )
+		{
+			final Module moduleInstance = injector.getInstance( moduleClass );
+			instances.add( moduleInstance );
+		}
+
+		// Finalize
+		return instances;
+	}
+
+	/**
+	 * This method is used to fetch and/or create (Guice) modules necessary
+	 * to fully construct this application.
+	 * <p>
+	 * The modules that are initialized in this method and added to the passed
+	 * List will be used to create the {@link com.google.inject.Injector} instance that is used in
+	 * the context of this application.
+	 * </p>
+	 *
+	 * @param modules A list of modules (initially empty) that shall be used to
+	 * create the injector to be used in the context of this application.
+	 */
+	protected abstract void initModules ( Collection<Class<? extends AMinecraftModule>> modules);
+
+	/**
 	 * Run before anything else. Read your config, create blocks, items, etc,
 	 * and register them with the GameRegistry.
 	 *
@@ -131,6 +149,7 @@ public abstract class AGuiceMod
 	 */
 	protected void preInit ( FMLPreInitializationEvent event )
 	{
+		this.log.info( "preinit begin" );
 		final File suggConfigFile = event.getSuggestedConfigurationFile();
 
 		this.configs.load( suggConfigFile );
@@ -139,6 +158,7 @@ public abstract class AGuiceMod
 		// super.getEntities().register();
 		// proxy.initSounds();
 		// proxy.initRenders();
+		this.log.info( "preinit end" );
 	}
 
 	/**
@@ -149,6 +169,7 @@ public abstract class AGuiceMod
 	 */
 	protected void init ( FMLInitializationEvent event )
 	{
+		this.log.info( "init begin" );
 		this.tileEntites.registerTileEntities();
 		// super.getItems().addNames();
 		// super.getBlocks().registerNames();
@@ -157,6 +178,7 @@ public abstract class AGuiceMod
 		// super.getGui().init( this );
 
 		MinecraftForge.EVENT_BUS.register( this );
+		this.log.info( "init end" );
 	}
 
 	/**
@@ -166,24 +188,7 @@ public abstract class AGuiceMod
 	 */
 	protected void modLoaded ( FMLPostInitializationEvent event )
 	{
-
-	}
-
-	private class bindInstanceClassModule extends AbstractModule
-	{
-		private final Class<AGuiceMod> clazz;
-
-		bindInstanceClassModule ( final Class<AGuiceMod> clazz )
-		{
-			super();
-			this.clazz = clazz;
-		}
-
-		@Override
-		protected void configure ()
-		{
-			final AnnotatedBindingBuilder<AGuiceMod> bindingBuilder = this.bind( this.clazz );
-			bindingBuilder.toInstance( AGuiceMod.this );
-		}
+		this.log.info( "loaded begin" );
+		this.log.info( "loaded end" );
 	}
 }
--- src/main/java/de/thatsich/intellie/IntelligentEnergistics.java
+++ src/main/java/de/thatsich/intellie/IntelligentEnergistics.java
@@ -1,43 +1,34 @@
 package de.thatsich.intellie;
 
-import com.google.inject.Inject;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import de.thatsich.common.AGuiceMod;
 import de.thatsich.common.AMinecraftModule;
-import de.thatsich.common.util.Logger;
 import de.thatsich.intellie.decorative.test.ModuleTest;
 
 import java.util.Collection;
-import java.util.LinkedList;
 
 @Mod(modid = "intellie", name = "IntelligentEnergistics", version = "${version}", dependencies = "required-after:Forge@[7.0,);required-after:FML@[5.0.5,);required-after:appliedenergistics2")
 public class IntelligentEnergistics extends AGuiceMod
 {
-	@Inject private Logger log;
-//	@Mod.Instance(value = "intellie")
-//	public static IntelligentEnergistics s_instance;
-//
-//	@SidedProxy(clientSide = "de.thatsich.intellie.common.proxies.ClientProxy", serverSide = "de.thatsich.intellie.common.proxies.CommonProxy")
-//	public static CommonProxy s_proxy;
+	//	@Mod.Instance(value = "intellie")
+	//	public static IntelligentEnergistics s_instance;
+	//
+	//	@SidedProxy(clientSide = "de.thatsich.intellie.common.proxies.ClientProxy", serverSide = "de.thatsich.intellie.common.proxies.CommonProxy")
+	//	public static CommonProxy s_proxy;
 
 	@Override
-	public Collection<Class<? extends AMinecraftModule>> initModules ()
+	protected void initModules ( Collection<Class<? extends AMinecraftModule>> modules )
 	{
-		final Collection<Class<? extends AMinecraftModule>> classModules = new LinkedList<>();
-
-		classModules.add( ModuleTest.class );
-
-		return classModules;
+		modules.add( ModuleTest.class );
 	}
 
 	@Override
 	@Mod.EventHandler
 	public void preInit ( FMLPreInitializationEvent event )
 	{
-		this.log.info( "PreInit " );
 		super.preInit( event );
 	}
 
