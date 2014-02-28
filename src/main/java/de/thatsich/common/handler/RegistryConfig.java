31292c0f726a3e2d64b8eb009cf63824181a02fe
thatsIch <badhard@arcor.de>
2014-02-28 13:14:04 +0100

    Cleanup

---
+++ src/main/java/de/thatsich/common/handler/RegistryConfig.java
@@ -0,0 +1,15 @@
+package de.thatsich.common.handler;
+
+import java.io.File;
+
+/**
+ * @author thatsIch
+ * @date 25.02.14.
+ */
+public class RegistryConfig
+{
+	public void load ( final File suggConfigFile )
+	{
+
+	}
+}
---
+++ src/main/java/de/thatsich/common/handler/RegistryItem.java
@@ -0,0 +1,10 @@
+package de.thatsich.common.handler;
+
+/**
+ * @author thatsIch
+ * @date 25.02.14.
+ */
+public class RegistryItem
+{
+	public void registerItems () {}
+}
