4f6c0401d85eb7a2eef9e3ffdd294607039dfa02
thatsIch <badhard@arcor.de>
2014-03-06 12:30:20 +0100

    Hooking up Items and Blocks in module

--- src/main/java/de/thatsich/common/AMinecraftModule.java
+++ src/main/java/de/thatsich/common/AMinecraftModule.java
@@ -1,26 +1,34 @@
 package de.thatsich.common;
 
-import de.thatsich.common.registries.RegistryBlock;
 import de.thatsich.common.module.IModule;
 import de.thatsich.common.module.block.ABlock;
 import de.thatsich.common.module.block.AContainerBlock;
-import de.thatsich.common.module.item.IItem;
+import de.thatsich.common.module.item.AItem;
+import de.thatsich.common.registries.RegistryBlock;
+import de.thatsich.common.registries.RegistryConfig;
+import de.thatsich.common.registries.RegistryItem;
+import de.thatsich.common.registries.RegistryTileEntity;
 
 import javax.inject.Inject;
 
 
 public abstract class AMinecraftModule implements IModule
 {
-	private final ABlock block;
+	@Inject private RegistryBlock blocks;
+	@Inject private RegistryConfig configs;
+	@Inject private RegistryItem items;
+	@Inject private RegistryTileEntity tileEntities;
 
-	@Inject
-	protected AMinecraftModule( IItem item, ABlock block, AContainerBlock containerBlock)
+	protected AMinecraftModule ( AItem item, ABlock block, AContainerBlock containerBlock )
 	{
-		this.block = block;
-	}
-	
-	@Inject
-	private void init( RegistryBlock blocks ) {
-		blocks.addBlock( this.block );
+		if (item != null) {
+			this.items.addItem( item );
+		}
+
+		if (block != null) {
+			this.blocks.addBlock( block );
+		}
+
+		// TODO configs and TEs
 	}
 }
--- src/main/java/de/thatsich/common/registries/RegistryItem.java
+++ src/main/java/de/thatsich/common/registries/RegistryItem.java
@@ -1,10 +1,13 @@
 package de.thatsich.common.registries;
 
+import de.thatsich.common.module.item.AItem;
+
 /**
  * @author thatsIch
  * @date 25.02.14.
  */
 public class RegistryItem
 {
+	public void addItem( AItem item ) {}
 	public void registerItems () {}
 }
