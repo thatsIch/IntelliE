d97e9d351cbf79109df5ec5cc7b832c5c998d185
thatsIch <badhard@arcor.de>
2014-03-16 02:56:34 +0100

    intersection package

---
+++ src/main/java/de/thatsich/intellie/applied/intelligences/functionality/intersection/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Intersection Block > Network Block but can define 3 channels with liquid, redstone, power, item
+ *
+ * @author thatsIch
+ * @date 16.03.14.
+ */
+package de.thatsich.intellie.applied.intelligences.functionality.intersection;
