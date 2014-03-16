bf7c08d60f6780e7a9de8f1c336f48c4315e894c
thatsIch <badhard@arcor.de>
2014-03-16 02:59:10 +0100

    builder package

---
+++ src/main/java/de/thatsich/intellie/applied/intelligences/functionality/builder/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Builder Block
+ * hook up pattern via Spatial Pylon
+ * provide it with network
+ *
+ * @author thatsIch
+ * @date 16.03.14.
+ */
+package de.thatsich.intellie.applied.intelligences.functionality.builder;
