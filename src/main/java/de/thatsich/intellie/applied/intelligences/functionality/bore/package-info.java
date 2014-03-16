7e720d2a26b125c26409305cd1c01d36ae661d84
thatsIch <badhard@arcor.de>
2014-03-16 03:00:11 +0100

    bore package

---
+++ src/main/java/de/thatsich/intellie/applied/intelligences/functionality/bore/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * bore constructed with pipes
+ * and need water to flood up the blocks
+ *
+ * @author thatsIch
+ * @date 16.03.14.
+ */
+package de.thatsich.intellie.applied.intelligences.functionality.bore;
