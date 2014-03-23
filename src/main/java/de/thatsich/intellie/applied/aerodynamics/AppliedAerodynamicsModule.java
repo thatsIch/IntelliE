9ee6f4100d289c3051652ae933a23aedfd6e6108
thatsIch <badhard@arcor.de>
2014-03-23 10:45:38 +0100

    Removed Factory Test since empty

--- src/main/java/de/thatsich/intellie/applied/aerodynamics/AppliedAerodynamicsModule.java
+++ src/main/java/de/thatsich/intellie/applied/aerodynamics/AppliedAerodynamicsModule.java
@@ -1,9 +1,13 @@
 package de.thatsich.intellie.applied.aerodynamics;
 
 import dagger.Module;
+import dagger.Provides;
+import de.thatsich.intellie.applied.aerodynamics.common.Aerodynamics;
 import de.thatsich.intellie.applied.aerodynamics.functionality.suite.chest.AeroChest;
 import de.thatsich.intellie.common.module.IModule;
 import de.thatsich.intellie.common.registries.RegistryModule;
+
+import javax.inject.Singleton;
 
 /**
  * @author thatsIch
@@ -12,5 +16,11 @@ import de.thatsich.intellie.common.registries.RegistryModule;
 @Module(injects = AeroChest.class, includes = RegistryModule.class, library = true)
 public class AppliedAerodynamicsModule implements IModule
 {
-
+	@Provides
+	@Singleton
+	@Aerodynamics
+	String provideAerodynamicsIdentifier ()
+	{
+		return "Aero";
+	}
 }
