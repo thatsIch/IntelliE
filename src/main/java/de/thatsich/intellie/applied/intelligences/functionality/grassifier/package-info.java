8b46f3e66e5cf756aed485af45dff7b74390a6b8
thatsIch <badhard@arcor.de>
2014-03-30 14:31:09 +0200

    Added grassifier idea

---
+++ src/main/java/de/thatsich/intellie/applied/intelligences/functionality/grassifier/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * ME Interface like auto refill
+ * 3 seeds + 1 water bottle converts dirt into gras
+ *
+ * @author thatsIch
+ * @since 30.03.2014.
+ */
+package de.thatsich.intellie.applied.intelligences.functionality.grassifier;
