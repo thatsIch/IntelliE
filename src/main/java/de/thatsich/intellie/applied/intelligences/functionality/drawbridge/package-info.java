5aead78d027bc71a376083a0433848f4e706c57f
thatsIch <badhard@arcor.de>
2014-03-30 14:30:15 +0200

    Added drawbridge idea

---
+++ src/main/java/de/thatsich/intellie/applied/intelligences/functionality/drawbridge/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * vllt mit inventory muster sodass man verschiedene Blöcke machen kann
+ * zB Stone, Dirt, Dirt, Stone
+ * vllt neue Feld öffnet sich, wenn man in erstes Feld einträgt
+ *
+ * @author thatsIch
+ * @since 30.03.2014.
+ */
+package de.thatsich.intellie.applied.intelligences.functionality.drawbridge;
