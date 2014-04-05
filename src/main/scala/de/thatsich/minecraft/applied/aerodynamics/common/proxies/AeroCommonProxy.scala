85fb15f2b93b4f34a13112c0348aba636cd841e6
thatsIch <badhard@arcor.de>
2014-04-05 12:30:09 +0200

    Add common interface for common proxies

--- src/main/scala/de/thatsich/minecraft/applied/aerodynamics/common/proxies/AeroCommonProxy.scala
+++ src/main/scala/de/thatsich/minecraft/applied/aerodynamics/common/proxies/AeroCommonProxy.scala
@@ -1,12 +1,14 @@
 package de.thatsich.minecraft.applied.aerodynamics.common.proxies
 
+import de.thatsich.minecraft.common.ICommonProxy
+
 /**
  *
  *
  * @author thatsIch
  * @since 04.04.2014.
  */
-class AeroCommonProxy
+class AeroCommonProxy extends ICommonProxy
 {
 
 }
---
+++ src/main/scala/de/thatsich/minecraft/common/ICommonProxy.scala
@@ -0,0 +1,14 @@
+package de.thatsich.minecraft.common
+
+/**
+ *
+ *
+ * @author thatsIch
+ * @since 05.04.2014.
+ */
+trait ICommonProxy
+{
+	def initSounds()
+
+	def initRenders()
+}
