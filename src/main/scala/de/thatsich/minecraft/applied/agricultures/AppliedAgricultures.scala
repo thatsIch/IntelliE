122d28fede3e152f3189670953c140c553a1bba3
thatsIch <badhard@arcor.de>
2014-04-05 13:38:12 +0200

    Declare others modules as mods

--- src/main/scala/de/thatsich/minecraft/applied/agricultures/AppliedAgricultures.scala
+++ src/main/scala/de/thatsich/minecraft/applied/agricultures/AppliedAgricultures.scala
@@ -1,11 +1,20 @@
 package de.thatsich.minecraft.applied.agricultures
 
+import cpw.mods.fml.common.Mod
+
 /**
  *
  *
  * @author thatsIch
  * @since 05.04.2014.
  */
+@Mod(
+	modid = "appagri",
+	name = "AppliedAgricultures",
+	version = "${version}",
+	dependencies = "required-after:intellie",
+	modLanguage = "scala"
+)
 object AppliedAgricultures
 {
 
--- src/main/scala/de/thatsich/minecraft/applied/energistics/AppliedIntelligences.scala
+++ src/main/scala/de/thatsich/minecraft/applied/energistics/AppliedIntelligences.scala
@@ -1,11 +1,20 @@
 package de.thatsich.minecraft.applied.energistics
 
+import cpw.mods.fml.common.Mod
+
 /**
  *
  *
  * @author thatsIch
  * @since 05.04.2014.
  */
+@Mod(
+	modid = "appint",
+	name = "AppliedIntelligences",
+	version = "${version}",
+	dependencies = "required-after:intellie",
+	modLanguage = "scala"
+)
 object AppliedIntelligences
 {
 
