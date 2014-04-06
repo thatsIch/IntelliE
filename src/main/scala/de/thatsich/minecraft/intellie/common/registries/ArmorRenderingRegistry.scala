8b2b6286a2facdb552236ea0d06c86c09279279b
thatsIch <badhard@arcor.de>
2014-04-07 01:26:06 +0200

    ported IBlock

---
+++ src/main/scala/de/thatsich/minecraft/intellie/common/registries/ArmorRenderingRegistry.scala
@@ -0,0 +1,25 @@
+package de.thatsich.minecraft.intellie.common.registries
+
+import de.thatsich.minecraft.intellie.common.logger.ILog
+import cpw.mods.fml.client.registry.RenderingRegistry
+
+/**
+ * Registry for armor rendering
+ * Add an armor to it which
+ * will be registered into the actual registry
+ * in pre-init automatically
+ *
+ * @author thatsIch
+ * @since 07.04.2014.
+ */
+class ArmorRenderingRegistry(log: ILog) extends ARegistry[ String ](log)
+{
+	def register()
+	{
+		for( armor <- this.armorRenderers )
+		{
+			RenderingRegistry.addNewArmourRendererPrefix(armor)
+		}
+		this.log.info("Registered all ArmorRenderers")
+	}
+}
---
+++ src/main/scala/de/thatsich/minecraft/intellie/common/registries/BlockContainerRegistry.scala
@@ -0,0 +1,14 @@
+package de.thatsich.minecraft.intellie.common.registries
+
+import de.thatsich.minecraft.intellie.common.logger.ILog
+
+/**
+ *
+ *
+ * @author thatsIch
+ * @since 07.04.2014.
+ */
+class BlockContainerRegistry(log: ILog) extends ARegistry[ IBlockContainer ](log)
+{
+
+}
