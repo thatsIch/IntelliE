c1f47b0cbd209b9e859062c2a7475378a77bd3e5
thatsIch <badhard@arcor.de>
2014-07-09 17:24:19 +0200

    Modinfo for AE2

---
+++ src/main/scala/de/thatsich/minecraft/api/interop/AE2ModInfo.scala
@@ -0,0 +1,15 @@
+package de.thatsich.minecraft.api.interop
+
+/**
+ *
+ *
+ * @author thatsIch
+ * @since 09.07.2014.
+ */
+object AE2ModInfo
+{
+	final val id          : String = "appliedenergistics2"
+	final val name        : String = "Applied Energistics 2"
+	final val version     : String = "${version}"
+	final val dependencies: String = ""
+}
