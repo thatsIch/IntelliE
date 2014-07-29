a15e888f6e8da773b2d6861062789df4699e774d
thatsIch <badhard@arcor.de>
2014-07-29 03:39:22 +0200

    * Fixing IE config NPE

--- src/main/scala/de/thatsich/minecraft/intellie/intern/unloadchilds/ChildUnloaderConfig.scala
+++ src/main/scala/de/thatsich/minecraft/intellie/intern/unloadchilds/ChildUnloaderConfig.scala
@@ -8,10 +8,11 @@ package de.thatsich.minecraft.intellie.intern.unloadchilds
  */
 trait ChildUnloaderConfig
 {
+	val config = new IEChildConfig( ConfigPath )
+
 	protected val disableAero = this.config.getBoolean( "ChildMods", "disableAppliedAerodynamics", defaultValue = false )
 	protected val disableAgro = this.config.getBoolean( "ChildMods", "disableAppliedAgricultures", defaultValue = false )
 	protected val disableInt  = this.config.getBoolean( "ChildMods", "disableAppliedIntelligences", defaultValue = false )
-	private val config = new IEChildConfig( ConfigPath )
 
 	this.config.save( )
 }
