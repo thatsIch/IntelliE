20dac634f89ba2a4ee17dadd1e33945076b53b9c
thatsIch <badhard@arcor.de>
2014-08-04 13:56:48 +0200

    + Basic texture ripped off AE2

--- src/main/scala/de/thatsich/minecraft/intellie/applied/aerodynamics/intern/functional/bench/WorkbenchBlock.scala
+++ src/main/scala/de/thatsich/minecraft/intellie/applied/aerodynamics/intern/functional/bench/WorkbenchBlock.scala
@@ -5,7 +5,8 @@ import net.minecraft.block.BlockContainer
 import net.minecraft.block.material.Material
 import net.minecraft.client.renderer.texture.IIconRegister
 import net.minecraft.tileentity.TileEntity
-import net.minecraft.world.World
+import net.minecraft.util.IIcon
+import net.minecraft.world.{IBlockAccess, World}
 
 /**
  *
@@ -15,13 +16,43 @@ import net.minecraft.world.World
  */
 class WorkbenchBlock extends BlockContainer( Material.iron )
 {
+	private var iconSide  : IIcon = null
+	private var iconTop   : IIcon = null
+	private var iconBottom: IIcon = null
+
 	this.setBlockName( "appaero.workbench" )
 	this.setBlockTextureName( "workbench" )
 
 	@SideOnly( Side.CLIENT )
-	override def registerBlockIcons( p_149651_1_ : IIconRegister ): Unit =
+	override def registerBlockIcons( register: IIconRegister ): Unit =
 	{
+		this.iconTop = register.registerIcon( "appaero:workbench_top" )
+		this.iconSide = register.registerIcon( "appaero:workbench_side" )
+		this.iconBottom = register.registerIcon( "appaero:workbench_bottom" )
+	}
 
+	@SideOnly( Side.CLIENT )
+	override def getIcon( workd: IBlockAccess, x: Int, y: Int, z: Int, side: Int ): IIcon =
+	{
+		side match
+		{
+			case 0 => this.iconBottom
+			case 1 => this.iconTop
+
+			case _ => this.iconSide
+		}
+	}
+
+	@SideOnly( Side.CLIENT )
+	override def getIcon( side: Int, meta: Int ): IIcon =
+	{
+		side match
+		{
+			case 0 => this.iconBottom
+			case 1 => this.iconTop
+
+			case _ => this.iconSide
+		}
 	}
 
 	def createNewTileEntity( p_149915_1_ : World, p_149915_2_ : Int ): TileEntity = new WorkbenchTileEntity
