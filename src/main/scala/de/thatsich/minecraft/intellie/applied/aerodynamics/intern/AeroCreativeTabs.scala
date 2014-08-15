30254a308da8ad00db9775cebb0882dfc8e2ffdc
thatsIch <badhard@arcor.de>
2014-08-15 20:57:12 +0200

    = Fix creative tab registrating

--- src/main/scala/de/thatsich/minecraft/intellie/applied/aerodynamics/intern/AeroCreativeTabs.scala
+++ src/main/scala/de/thatsich/minecraft/intellie/applied/aerodynamics/intern/AeroCreativeTabs.scala
@@ -1,11 +1,10 @@
 package de.thatsich.minecraft.intellie.applied.aerodynamics.intern
 
 
-import cpw.mods.fml.relauncher.{SideOnly, Side}
+import cpw.mods.fml.relauncher.{Side, SideOnly}
 import de.thatsich.minecraft.common.log.Log
 import de.thatsich.minecraft.common.module.Module
 import de.thatsich.minecraft.common.string.ID
-import net.minecraft.block.Block
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.Item
 
@@ -18,10 +17,19 @@ import net.minecraft.item.Item
  */
 class AeroCreativeTabs(icon: Item, registrable: Seq[Module], log: Log, modid: ID) extends CreativeTabs(modid)
 {
-	for (module: Module <- this.registrable; block: Block <- module.blocks; item: Item <- module.items)
+	for (module: Module <- this.registrable)
 	{
-		item.setCreativeTab(this)
-		block.setCreativeTab(this)
+		this.log.debug(s"Scanning ${module.getClass.getSimpleName} for blocks and items.")
+		module.blocks.foreach(block =>
+		{
+			this.log.debug(s"Add block ${block.getClass.getSimpleName} to this creative tab")
+			block.setCreativeTab(this)
+		})
+		module.items.foreach(item =>
+		{
+			this.log.debug(s"Add item ${item.getClass.getSimpleName} to this creative tab")
+			item.setCreativeTab(this)
+		})
 	}
 
 	@SideOnly(Side.CLIENT)
@@ -30,7 +38,8 @@ class AeroCreativeTabs(icon: Item, registrable: Seq[Module], log: Log, modid: ID
 		this.icon
 	}
 
-	override def getTranslatedTabLabel: String = {
+	override def getTranslatedTabLabel: String =
+	{
 		val id: String = this.modid
 
 		s"$id.tab.name"
