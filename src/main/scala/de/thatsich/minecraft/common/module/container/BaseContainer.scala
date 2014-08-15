6aee11ce1a99896e12df14f0672480a70d0fe5e8
thatsIch <badhard@arcor.de>
2014-08-15 22:24:04 +0200

    ~ Moved code to base container

--- src/main/scala/de/thatsich/minecraft/common/module/container/BaseContainer.scala
+++ src/main/scala/de/thatsich/minecraft/common/module/container/BaseContainer.scala
@@ -2,8 +2,9 @@ package de.thatsich.minecraft.common.module.container
 
 
 import de.thatsich.minecraft.common.module.container.slot.BaseSlot
-import net.minecraft.entity.player.InventoryPlayer
+import net.minecraft.entity.player.{EntityPlayer, InventoryPlayer}
 import net.minecraft.inventory.{Container, Slot}
+import net.minecraft.item.ItemStack
 
 
 /**
@@ -32,4 +33,68 @@ abstract class BaseContainer extends Container
 			this.addSlotToContainer(new Slot(player, col, 8 + 18 * col + offsetX, 58 + offsetY))
 		}
 	}
+
+	override def transferStackInSlot(player: EntityPlayer, slotIndex: Int): ItemStack =
+	{
+		var resultStack: ItemStack = null
+		val clickedSlot = this.getSlot(slotIndex)
+
+		if (clickedSlot != null && clickedSlot.getHasStack)
+		{
+			val clickedStack = clickedSlot.getStack
+			resultStack = clickedStack.copy()
+
+			// in inventory
+			if (slotIndex < 36)
+			{
+				val machineInventorySize = this.inventorySlots.size()
+				for (machineSlotIndex <- 36 until machineInventorySize)
+				{
+					val machineSlot = this.getBaseSlot(machineSlotIndex)
+					if (machineSlot != null & !machineSlot.getHasStack && machineSlot.isItemValid(clickedStack))
+					{
+						val limit = machineSlot.getSlotStackLimit
+						if (clickedStack.stackSize > limit)
+						{
+							val splitStack: ItemStack = clickedStack.splitStack(limit)
+							if (!this.mergeItemStack(splitStack, machineSlotIndex, machineSlotIndex + 1, true))
+							{
+								return null
+							}
+						}
+						else
+						{
+							if (!this.mergeItemStack(clickedStack, 36, machineInventorySize, true))
+							{
+								return null
+							}
+						}
+					}
+				}
+			}
+
+			// in machine
+			else
+			{
+				if (!this.mergeItemStack(clickedStack, 0, 36, false))
+				{
+					return null
+				}
+			}
+
+			if (clickedStack.stackSize == 0)
+			{
+				clickedSlot.putStack(null)
+			}
+			else
+			{
+				clickedSlot.onSlotChanged()
+			}
+		}
+
+		resultStack
+	}
+
+	override def retrySlotClick(p_75133_1_ : Int, p_75133_2_ : Int, p_75133_3_ : Boolean, player: EntityPlayer): Unit =
+	{}
 }
--- src/main/scala/de/thatsich/minecraft/intellie/applied/aerodynamics/intern/module/bench/WorkbenchContainer.scala
+++ src/main/scala/de/thatsich/minecraft/intellie/applied/aerodynamics/intern/module/bench/WorkbenchContainer.scala
@@ -15,7 +15,6 @@ import de.thatsich.minecraft.common.module.container.slot.SlotSide.SlotSide
 import de.thatsich.minecraft.intellie.applied.aerodynamics.intern.module.bench.client.{InputSlot, UpgradeSlot}
 import net.minecraft.entity.player.{EntityPlayer, InventoryPlayer}
 import net.minecraft.inventory.ICrafting
-import net.minecraft.item.ItemStack
 
 import scala.collection.JavaConversions._
 
@@ -70,68 +69,4 @@ class WorkbenchContainer(player: InventoryPlayer, workbench: WorkbenchTileEntity
 
 		this.lastModificationTime = this.workbench.modificationTime
 	}
-
-	override def transferStackInSlot(player: EntityPlayer, slotIndex: Int): ItemStack =
-	{
-		var resultStack: ItemStack = null
-		val clickedSlot = this.getSlot(slotIndex)
-
-		if (clickedSlot != null && clickedSlot.getHasStack)
-		{
-			val clickedStack = clickedSlot.getStack
-			resultStack = clickedStack.copy()
-
-			// in inventory
-			if (slotIndex < 36)
-			{
-				val machineInventorySize = this.inventorySlots.size()
-				for (machineSlotIndex <- 36 until machineInventorySize)
-				{
-					val machineSlot = this.getBaseSlot(machineSlotIndex)
-					if (machineSlot != null & !machineSlot.getHasStack && machineSlot.isItemValid(clickedStack))
-					{
-						val limit = machineSlot.getSlotStackLimit
-						if (clickedStack.stackSize > limit)
-						{
-							val splitStack: ItemStack = clickedStack.splitStack(limit)
-							if (!this.mergeItemStack(splitStack, machineSlotIndex, machineSlotIndex + 1, true))
-							{
-								return null
-							}
-						}
-						else
-						{
-							if (!this.mergeItemStack(clickedStack, 36, machineInventorySize, true))
-							{
-								return null
-							}
-						}
-					}
-				}
-			}
-
-			// in machine
-			else
-			{
-				if (!this.mergeItemStack(clickedStack, 0, 36, false))
-				{
-					return null
-				}
-			}
-
-			if (clickedStack.stackSize == 0)
-			{
-				clickedSlot.putStack(null)
-			}
-			else
-			{
-				clickedSlot.onSlotChanged()
-			}
-		}
-
-		resultStack
-	}
-
-	override def retrySlotClick(p_75133_1_ : Int, p_75133_2_ : Int, p_75133_3_ : Boolean, player: EntityPlayer): Unit =
-	{}
 }
