c507a22fa4edfc6d26aeb5868037205bfa1ac2ea
thatsIch <badhard@arcor.de>
2014-08-16 22:44:39 +0200

    Refactoring

--- src/main/scala/de/thatsich/minecraft/common/module/container/slot/SlotState.scala
+++ src/main/scala/de/thatsich/minecraft/common/module/container/slot/SlotState.scala
@@ -7,7 +7,7 @@ package de.thatsich.minecraft.common.module.container.slot
  * @author thatsIch
  * @since 16.08.2014.
  */
-object SlotStateWTF extends Enumeration
+object SlotState extends Enumeration
 {
 	type SlotState = Value
 	val Disabled, Enabled, Fake = Value
