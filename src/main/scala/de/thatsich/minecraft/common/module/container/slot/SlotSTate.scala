5b9a2ee932fdf7430e2edbe21efb8fba7e756e80
thatsIch <badhard@arcor.de>
2014-08-16 12:25:55 +0200

    pls?

---
+++ src/main/scala/de/thatsich/minecraft/common/module/container/slot/SlotSTate.scala
@@ -0,0 +1,13 @@
+package de.thatsich.minecraft.common.module.container.slot
+
+/**
+ *
+ *
+ * @author thatsIch
+ * @since 16.08.2014.
+ */
+object SlotState extends Enumeration
+{
+	type SlotState = Value
+	val Disabled, Enabled, Fake = Value
+}
