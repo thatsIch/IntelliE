94d7335955bcde08302bd82734de15df85fbc88d
thatsIch <badhard@arcor.de>
2014-08-29 16:14:45 +0200

    = Dont draw button of progress bar in workbench

--- src/main/scala/de/thatsich/minecraft/common/module/gui/GuiProgressBar.scala
+++ src/main/scala/de/thatsich/minecraft/common/module/gui/GuiProgressBar.scala
@@ -14,21 +14,17 @@ import net.minecraft.util.ResourceLocation
  * @since 20.08.2014.
  */
 class GuiProgressBar(texture: ResourceLocation,
-                     xPos: Int,
-                     yPos: Int,
+                     xPosition: Int,
+                     yPosition: Int,
                      fillU: Int,
                      fillV: Int,
-                     _width: Int,
-                     _height: Int,
+                     width: Int,
+                     height: Int,
                      direction: Direction,
-                     val maxValue: Int) extends GuiButton(xPos, yPos, _width, "")
+                     val maxValue: Int) extends GuiButton(xPosition, yPosition, width, "")
                                                 with ToolTip
 {
 	var currentValue = 0
-	this.xPosition = this.xPos
-	this.yPosition = this.yPos
-	this.width = 0
-	this.height = this._height
 
 	override def drawButton(mc: Minecraft, x: Int, y: Int): Unit =
 	{
@@ -39,12 +35,12 @@ class GuiProgressBar(texture: ResourceLocation,
 			this.direction match
 			{
 				case Direction.Vertical =>
-					val diff = this._height - (if (this.maxValue > 0) this._height * this.currentValue / this.maxValue else 0)
-					this.drawTexturedModalRect(this.xPos, this.yPos + diff, this.fillU, this.fillV + diff, this._width, this._height - diff)
+					val diff = this.height - (if (this.maxValue > 0) this.height * this.currentValue / this.maxValue else 0)
+					this.drawTexturedModalRect(this.xPosition, this.yPosition + diff, this.fillU, this.fillV + diff, this.width, this.height - diff)
 
 				case Direction.Horizontal =>
-					val diff = this._width - (if (this.maxValue > 0) this._width * this.currentValue / this.maxValue else 0)
-					this.drawTexturedModalRect(this.xPos, this.yPos, this.fillU + diff, this.fillV, this._width - diff, this._height)
+					val diff = this.width - (if (this.maxValue > 0) this.width * this.currentValue / this.maxValue else 0)
+					this.drawTexturedModalRect(this.xPosition, this.yPosition, this.fillU + diff, this.fillV, this.width - diff, this.height)
 			}
 
 			this.mouseDragged(mc, x, y)
@@ -56,11 +52,11 @@ class GuiProgressBar(texture: ResourceLocation,
 
 	def toolTipIsVisisble: Boolean = true
 
-	def toolTipXActivationPos: Int = this.xPos - 2
+	def toolTipXActivationPos: Int = this.xPosition - 2
 
-	def toolTipActivationWidth: Int = this._width + 4
+	def toolTipActivationWidth: Int = this.width + 4
 
-	def toolTipYActivationPos: Int = this.yPos - 2
+	def toolTipYActivationPos: Int = this.yPosition - 2
 
-	def toolTipActivationHeight: Int = this._height + 4
+	def toolTipActivationHeight: Int = this.height + 4
 }
