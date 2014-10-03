4e13ac373d0d22c3bac46cff5cf81f96f38032be
thatsIch <badhard@arcor.de>
2014-10-03 12:12:11 +0200

    Disabling childunloading temporary to fix #10

--- src/main/scala/de/thatsich/minecraft/intellie/IntelligentEnergistics.scala
+++ src/main/scala/de/thatsich/minecraft/intellie/IntelligentEnergistics.scala
@@ -1,14 +1,7 @@
 package de.thatsich.minecraft.intellie
 
 
-import cpw.mods.fml.common.{Loader, Mod}
-import de.thatsich.minecraft.common.log.SimpleLog
-import de.thatsich.minecraft.common.util.string.BaseAbbreviation
-import de.thatsich.minecraft.intellie.applied.aerodynamics.AppliedAerodynamics
-import de.thatsich.minecraft.intellie.applied.aeronei.AppliedAerodynamicsNei
-import de.thatsich.minecraft.intellie.applied.agricultures.AppliedAgricultures
-import de.thatsich.minecraft.intellie.applied.intelligences.AppliedIntelligences
-import de.thatsich.minecraft.intellie.child.{ChildUnloader, ChildUnloaderConfigAccess}
+import cpw.mods.fml.common.Mod
 
 
 /**
@@ -24,18 +17,18 @@ import de.thatsich.minecraft.intellie.child.{ChildUnloader, ChildUnloaderConfigA
 	dependencies = IntelligentEnergistics.dependencies,
 	modLanguage = "scala"
 )
-object IntelligentEnergistics extends ChildUnloader with ChildUnloaderConfigAccess
+object IntelligentEnergistics //extends ChildUnloader with ChildUnloaderConfigAccess
 {
 	final val id = "intellie"
 	final val name = "Intelligent Energistics"
 	final val version = "@version@"
 	final val dependencies = "required-after:Forge@[7.0,);required-after:FML@[5.0.5,);required-after:appliedenergistics2@[rv1-beta-1,);after:NotEnoughItems"
 
-	private val log = new SimpleLog(new BaseAbbreviation("IE"))
+//	private val log = new SimpleLog(new BaseAbbreviation("IE"))
 
-	this.unload(AppliedAerodynamics.id, this.disableAero, this.log)
-	this.unload(AppliedAgricultures.id, this.disableAgro, this.log)
-	this.unload(AppliedIntelligences.id, this.disableInt, this.log)
-
-	this.unload(AppliedAerodynamicsNei.id, this.disableAero || this.disableAeroNEI || this.disableNEI || !Loader.isModLoaded("NotEnoughItems"), this.log)
+//	this.unload(AppliedAerodynamics.id, this.disableAero, this.log)
+//	this.unload(AppliedAgricultures.id, this.disableAgro, this.log)
+//	this.unload(AppliedIntelligences.id, this.disableInt, this.log)
+//
+//	this.unload(AppliedAerodynamicsNei.id, this.disableAero || this.disableAeroNEI || this.disableNEI || !Loader.isModLoaded("NotEnoughItems"), this.log)
 }
